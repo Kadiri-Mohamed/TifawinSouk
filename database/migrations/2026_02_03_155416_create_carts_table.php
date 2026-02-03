ffc15c4cf6a2ccb16a93fd0367227c0e3604166b
Hajar <elmouhili.h044@ucd.ac.ma>
2026-02-03 17:10:22 +0100

    fix:modifier le mot table par create dans les migrations

--- database/migrations/2026_02_03_155416_create_carts_table.php
+++ database/migrations/2026_02_03_155416_create_carts_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('carts', function (Blueprint $table) {
+        Schema::create('carts', function (Blueprint $table) {
             $table->id(); 
             $table->foreignId('user_id')->unique()->constrained()->cascadeOnDelete();
             $table->timestamps();
--- database/migrations/2026_02_03_155649_create_cart_items_table.php
+++ database/migrations/2026_02_03_155649_create_cart_items_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('cart_items', function (Blueprint $table) {
+        Schema::create('cart_items', function (Blueprint $table) {
                      $table->id(); 
 
             $table->foreignId('cart_id')->constrained()->onDelete('cascade');
