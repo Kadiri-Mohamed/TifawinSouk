db00e1a512834e73f7cb13559d2a162526d42a2b
ali kara <ali16kara@gmail.com>
2026-02-05 15:41:42 +0100

    feat: implement admin categories CRUD views

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -1,3 +1,33 @@
-<div>
-    <!-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk -->
+<h1>Create Category</h1>
+
+@if ($errors->any())
+<div style="color: red;">
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
 </div>
+@endif
+
+<form action="{{ route('admin.categories.store') }}" method="POST">
+    @csrf
+    <div>
+        <label for="name">Name:</label><br>
+        <input type="text" id="name" name="name" value="{{ old('name') }}" required>
+    </div>
+    <br>
+    <div>
+        <label for="slug">Slug:</label><br>
+        <input type="text" id="slug" name="slug" value="{{ old('slug') }}" required>
+    </div>
+    <br>
+    <div>
+        <label for="description">Description:</label><br>
+        <textarea id="description" name="description">{{ old('description') }}</textarea>
+    </div>
+    <br>
+    <button type="submit">Create</button>
+</form>
+
+<a href="{{ route('admin.categories.index') }}">Back to List</a>
