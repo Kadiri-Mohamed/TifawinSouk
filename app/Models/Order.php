f290d46bf953de596c7f8162b63e1ad0510db74d
Kadiri <kadiri.moohamed@gmail.com>
2026-02-06 15:59:35 +0100

    feat: add orderItems relation to Order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -21,4 +21,8 @@ class Order extends Model
     {
         return $this->belongsTo(User::class);
     }
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
