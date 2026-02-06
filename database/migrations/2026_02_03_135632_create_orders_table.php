b058b0f4511cee231da9330336030a158ca74cf7
Kadiri <kadiri.moohamed@gmail.com>
2026-02-06 11:42:53 +0100

    feat: add admin orders controller and soft deletes

--- database/migrations/2026_02_03_135632_create_orders_table.php
+++ database/migrations/2026_02_03_135632_create_orders_table.php
@@ -17,6 +17,7 @@ return new class extends Migration {
             $table->boolean('is_paid')->default(false);
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
